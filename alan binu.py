cc8c641294265d9b09ca7aad26dba4098dc1d82d
ALANBINU0 <alanbinut@gmail.com>
2024-10-04 16:11:51 +0530

    Add files via upload

---
+++ alan binu.py
@@ -0,0 +1,49 @@
+#print('hello,world!')
+#print('hello\nworld')
+'''
+if 5>2
+   print('five is greater than two!')
+if 5>2:
+    print('five is greater than two!')
+if 5>2:
+    print('five is greater than two!')
+
+x=5
+x="thomas"
+print(x)
+
+
+x=int(3)
+y=int(3)
+z=float(3)
+print(x)
+print(y)
+print(z)
+x=5
+y="John"
+print(type(x))
+print(type(y))
+
+x,y,z= 'orange','banana','cherry'
+print(x)
+print(y)
+print(z)
+
+x='python '
+y='is '
+z='awesome '
+print(x+y+z)
+
+username=input('enter username')
+print('username is:' + username)
+
+a='hello'
+b='world'
+c=a+b
+print(c)
+
+a='hello,world!'
+print(a[0])
+'''
+b='hello,world!'
+print(b[:5])
