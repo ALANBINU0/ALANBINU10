8b123f1ddd8abafa83127944fcca884739da700d
ALANBINU0 <alanbinut@gmail.com>
2024-10-18 14:46:16 +0530

    Add files via upload

---
+++ basicinput1.py
@@ -0,0 +1,15 @@
+'''
+Author: Alan Binu
+Date:18-10-2024
+Description:
+
+a Python program that asks the user to input their name and age.
+ After receiving the input,
+  display a message that includes the user's name and age.
+hello, alan ! you are 18 years old
+'''
+
+name=input('enter your name')
+age=input('enter your age')
+print("hello,",name,"! you are",age,"years old")
+
