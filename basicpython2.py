68747720dc7cd32692b9abb8401c672333217986
ALANBINU0 <alanbinut@gmail.com>
2024-10-18 15:07:17 +0530

    Add files via upload

---
+++ basicpython2.py
@@ -0,0 +1,14 @@
+'''
+Author:Alan Binu
+Date:18-10-2024
+Description=Python program that stores a string in a variable.
+ Extract a specific part of the string (substring) and then concatenate it with another string.
+  Finally, display the new string.
+'''
+str1='Hello, World!'
+substr=str1[7:12]
+print(substr)
+str2=" Everyone!"
+substr1=substr+str2
+print(substr1)
+
