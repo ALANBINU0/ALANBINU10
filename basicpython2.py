b14594e5ac8e6744bd697f0eb26f11ba6420c167
ALANBINU0 <alanbinut@gmail.com>
2024-10-18 15:24:44 +0530

    Update basicpython2.py

--- basicpython2.py
+++ basicpython2.py
@@ -4,6 +4,7 @@ Date:18-10-2024
 Description=Python program that stores a string in a variable.
  Extract a specific part of the string (substring) and then concatenate it with another string.
   Finally, display the new string.
+  World Everyone!
 '''
 str1='Hello, World!'
 substr=str1[7:12]
