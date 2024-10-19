9d3d6d60e7f314b9c36ce5bc9e44c18d267d7952
ALANBINU0 <alanbinut@gmail.com>
2024-10-19 15:18:24 +0530

    Add files via upload

---
+++ basicpython6.py
@@ -0,0 +1,26 @@
+'''
+
+Author:Alan Binu
+Date:19-10-2024
+Description:a Python program that performs the following tasks:
+
+    Create two separate strings:
+        The first string should contain your first name.
+        The second string should contain your last name.
+
+    Concatenate the two strings with a space in between and store the result in a new variable.
+
+    Print the concatenated string.
+
+    From the concatenated string:
+        Access and print a sub-string that consists of the last name only.
+         Use string slicing to extract the sub-string.
+'''
+first_name=input("enter your first name:")
+last_name=input("enter your last name:")
+name=first_name+" "+last_name
+print(name)
+print(len(first_name))
+first_name_length=len(first_name)
+extracted_last_name=name[first_name_length+1:]
+print(extracted_last_name)
