d0c75c66b2c3a42edac62fc05dd5bd007958f395
ALANBINU0 <alanbinut@gmail.com>
2024-10-25 14:52:29 +0530

    Add files via upload

---
+++ basicpython7.py
@@ -0,0 +1,19 @@
+'''
+Name=Alan Binu
+Date:25-10-2024
+Description:
+a Python program to find the largest of three numbers.
+The program should take three numbers as input from the user and determine which one is the largest.
+Use conditional statements to compare the numbers.
+'''
+
+
+
+
+number1=int(input('enter the first number'))
+number2=int(input('enter the second number'))
+if (number1>number2):
+    print(number1,'is greater than',number2)
+else:
+    print(number2,'is greater than',number1)
+
---
+++ basicpython8.py
@@ -0,0 +1,21 @@
+
+number1=int(input('enter the first number'))
+number2=int(input('enter the second number'))
+if number1>number2:
+    print(number1,'is greater than',number2)
+else:
+    print(number2,'is greater than',number1)
+
+if number1>0:
+    print(number1,'is a positive number')
+elif number1<0:
+    print(number1,'is a negative number')
+else:
+    print(number1,'is a zero')
+if number2>0:
+    print(number2,'is positive number')
+elif number2<0:
+    print(number2,'is negative number')
+else:
+    print(number2,'is a zero')
+
