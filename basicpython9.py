3694a02c8114b8f7647d73e63b6e7e884204c81a
ALANBINU0 <alanbinut@gmail.com>
2024-10-25 15:59:30 +0530

    basicpython9.py

--- basicpython9.py
+++ basicpython9.py
@@ -4,6 +4,7 @@ Date:25-10-2024
 Description:a Python program to convert temperature values back and forth between Celsius and Fahrenheit.
 The user should be able to input a temperature in Celsius or Fahrenheit,
 and the program should convert it to the other unit using the formula:
+c/5=(f-32)/9
 '''
 temp=int(input('enter the temperature'))
 scale=input(' is this in (C)elsius or (F)ahrenheit?')
